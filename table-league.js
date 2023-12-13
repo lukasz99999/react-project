790eff68e312f09a496c33db13995ff632a04788
lukasz <110409416+lukasz99999@users.noreply.github.com>
2023-12-13 18:21:47 +0100

    Update table-league.js

--- table-league.js
+++ table-league.js
@@ -1,8 +1,39 @@
+import React, { useState, useEffect } from "react";
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
+} from "react-bootstrap";
 
-        ))}
-      </tbody>
-    </Table>
+const TableMatches = () => {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://php74.appgo.pl/sport_api/api/public/api/games?page=1&onPage=5&orderDirection=desc&orderBy=round"
+      );
+      const data = await response.json();
+      setMatches(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <ListGroup>
+      {matches.map((match, index) => (
+        <ListGroupItem key={index}>
+          <ListGroupItemHeading>
+            {match.round} - {match.date}
+          </ListGroupItemHeading>
+          <ListGroupItemText>
+            {match.homeTeam} {match.homeScore} - {match.awayTeam} {match.awayScore}
+          </ListGroupItemText>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
   );
 };
 
-export default TableLeague;
+export default TableMatches;
